12bc449597ef0ae60520566100a52bec23307107
feverGeek <1265179960@qq.com>
2019-04-26 16:00:53 +0800

    修改adc.h

--- adc.h
+++ adc.h
@@ -3,11 +3,6 @@
 
 #include "sys.h" 
  							   
-void MY_ADCx_Regular_Init(uint32_t RCC_GPIOxPeriph, uint32_t GPIO_Pinx, uint32_t RCC_ADCxPeriph, uint8_t ADC_Channel);
-void MY_ADCx_Regular_Double_Channel_Init(uint32_t RCC_GPIOxPeriph, uint32_t GPIO_Pinx1, uint32_t GPIO_Pinx2, uint32_t RCC_ADCxPeriph, uint8_t ADC_Channel1, uint8_t ADC_Channel2);
-void DMA_Configuration(void);
-u16 Get_ADC_Value(ADC_TypeDef* ADCx);
-u16 Get_ADC_Average(ADC_TypeDef* ADCx, u8 times);
 
 //ADC序号的宏定义  
 #define RHEOSTAT_ADC              ADC3  
